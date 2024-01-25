a05932c83da0a9c0f6782d667a0b8b10c630d121
robo <unconfigured@null.spigotmc.org>
2024-01-25 15:30:26 +0100

    All builtins (except for export and unset) mostly working, and without leaks

--- builtins_echo.c
+++ builtins_echo.c
@@ -6,38 +6,60 @@
 /*   By: glambrig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:12:13 by glambrig          #+#    #+#             */
-/*   Updated: 2024/01/25 14:02:31 by glambrig         ###   ########.fr       */
+/*   Updated: 2024/01/25 15:26:51 by glambrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*compare_env_var_with_envp(char *s, char **envp)
+/*Return value is allocated with malloc and MUST be freed after use.*/
+char	*compare_env_var_with_envp(char *str, char **envp)
 {
-	int	i;
+	int		i;
+	char	*s;
+	char	**env_split;
+	char	*res;
 
 	i = 0;
+	s = ft_strtrim(str, "$");
 	while (envp[i])
 	{
-		if (strncmp(s, envp[i], ft_strlen(envp[i])) == 0)
-			return (envp[i]);
+		if (strncmp(s, envp[i], ft_strlen(s)) == 0)
+		{
+			env_split = ft_split(envp[i], '=');
+			res = ft_strdup(env_split[1]);
+			free_tokens(env_split);
+			free(s);
+			return (res);
+		}
 		i++;
 	}
+	free(s);
 	return (NULL);
 }
 
+/*Checks if the given string 's' is an environment variable*/
 int	check_if_env_var(char *s, char **envp)
 {
-	if (ft_strchr(s, '$') == 0)
+	char	*free_v;
+
+	if (ft_strchr(s, '$') != NULL)
 	{
 		if (s[0] != '$')
 			return (0);
 		else
 		{
-			if (compare_env_var_with_envp(s, envp) != NULL)
+			free_v = compare_env_var_with_envp(s, envp);
+			if (free_v != NULL)
+			{
+				free(free_v);
 				return (1);
+			}
 			else
+			{
+				free(free_v);
 				return (2);
+			}
 		}
 	}
 	return (0);
@@ -46,16 +68,14 @@ int	check_if_env_var(char *s, char **envp)
 /*Make sure to pass the ENTIRE line (got by readline()),
 	otherwise the function might not work properly.*/
 /*TODO:
-	handle environment variables passed as input, e.g. "echo $USER"
 	echo $$ prints shell's PID
 	echo $? prints exit status of last command
-	echo "hello" "this is a test" "aaa" prints without the quotes
 */
 void	ft_echo(char *s, char **envp)
 {
-	(void)envp;
 	char	**tokens;
-	char *trimmed;
+	char 	*trimmed;
+	char	*result;
 	int		last;
 	int		i;
 
@@ -78,6 +98,14 @@ void	ft_echo(char *s, char **envp)
 		while (tokens[i])
 		{
 			trimmed = ft_strtrim(tokens[i++], "\"");
+			if (check_if_env_var(trimmed, envp) == 1)
+			{
+				result = compare_env_var_with_envp(trimmed, envp);
+				printf("%s ", result);
+				free(trimmed);
+				free(result);
+				continue ;
+			}
 			if (i == last - 1)
 				printf("%s", trimmed);
 			else
@@ -93,6 +121,14 @@ void	ft_echo(char *s, char **envp)
 		while (tokens[i])
 		{
 			trimmed = ft_strtrim(tokens[i++], "\"");
+			if (check_if_env_var(trimmed, envp) == 1)
+			{
+				result = compare_env_var_with_envp(trimmed, envp);
+				printf("%s ", result);
+				free(trimmed);
+				free(result);
+				continue ;
+			}
 			printf("%s ", trimmed);
 			free(trimmed);
 		}
