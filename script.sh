feb677292ad7fbcd8d7ec30cbfd95313bf0bdc25
de_la_verge <verge.f@gmail.com>
2024-01-26 13:13:13 +0100

    update final scripts okay

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-echo "alias gomaster=""git switch master""" >> ~/.zshrc
-echo "alias gogeorge=""git switch glambrig""" >> ~/.zshrc
-echo "alias goflo=""git switch flo_branch""" >> ~/.zshrc
-
-zsh
+echo "alias gomaster=""'git switch master'""" >> ~/.zshrc
+echo "alias gogeorge=""'git switch glambrig'""" >> ~/.zshrc
+echo "alias goflo=""'git switch flo_branch'""" >> ~/.zshrc
