11010ef17e653bd1cb86ba0ebdd61478669f03b4
flverge_42 <verge.f@gmail.com>
2024-01-31 09:59:52 +0100

    start testing spliiit

--- src/parsing/testing.c
+++ src/parsing/testing.c
@@ -13,9 +13,70 @@
 # include <readline/history.h>
 
 
-bool unclosed_quotes(char *str) // e"c'h"o ==> ec'ho, is then technically a valid argument
+#include "../../minishell.h"
+
+static char	*ft_strncpy(char *dest, char const *src, size_t n)
 {
-	size_t i;
+	size_t	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+static void	sub_check(char const *s, char c, size_t *i, size_t *start)
+{
+	while (s[*i] == c)
+		(*i)++;
+	if (s[*i] != c && s[*i])
+	{
+		*start = *i;
+		while (s[*i] != c && s[*i])
+			(*i)++;
+	}
+}
+
+static void	allocation(char **buffer, char const *s, char c, size_t len_s)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	start = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		sub_check(s, c, &i, &start);
+		if (j < len_s)
+		{
+			buffer[j] = ft_calloc(sizeof(char), (i - start + 1));
+			if (!buffer[j])
+				return ;
+			ft_strncpy(buffer[j], &s[start], i - start);
+			j++;
+		}
+		while (s[i] == c && s[i])
+			i++;
+	}
+}
+
+// split the words, join them later
+static size_t	parsing_countwords(char const *str, char c)
+{
+	size_t	result;
+	int		i;
 	char starting_quote;
 	char closing_quote;
 
@@ -25,28 +86,17 @@ bool unclosed_quotes(char *str) // e"c'h"o ==> ec'ho, is then technically a vali
 		starting_quote = 0;
 		closing_quote = 0;
 
-		// ! Step one, run accross non quote chars
+		// skip whitespaces
+		
+
 		while (str[i] != D_QUOTE && str[i] != S_QUOTE && str[i])
 			i++;
 		
-		if (str[i] == D_QUOTE) // if the current char is a double quote
+		if (str[i] == D_QUOTE || str[i] == S_QUOTE) // if the current char is a double quote
 		{
 			starting_quote = str[i];
 			i++;
-			while (str[i] && starting_quote != closing_quote)
-			{
-				if (str[i] == starting_quote)
-				{
-					closing_quote = starting_quote;
-					break ;
-				}
-				i++;
-			}
-		}
-		else if (str[i] == S_QUOTE) // if the current char is a single quote
-		{
-			starting_quote = str[i];
-			i++;
+			result++;
 			while (str[i] && starting_quote != closing_quote)
 			{
 				if (str[i] == starting_quote)
@@ -58,30 +108,25 @@ bool unclosed_quotes(char *str) // e"c'h"o ==> ec'ho, is then technically a vali
 			}
 		}
 		i++;
-		
 	}
-	if (starting_quote != closing_quote)
-		return (true);
-	return (false);
+	return (result);
 }
 
-int main(int ac, char **av, char **envp)
+char	**parsing_split(char const *s, char c)
 {
-	(void)ac;
-	(void)av;
-	char *str;
+	char	**buffer;
+	size_t	len_s;
 
-	str = readline("MINISHELL $ ");
+	len_s = parsing_countwords(s, c);
+	buffer = (char **)ft_calloc(sizeof(char *), (len_s + 1));
+	if (!buffer)
+		return (NULL);
+	allocation(buffer, s, c, len_s);
+	return (buffer);
+}
 
-	printf("str = %s\n", str);
-	if (unclosed_quotes(str))
-		printf("result of unclosed quote = TRUE");
-	else
-		printf("result of unclosed quote = FALSE");
-		
-
-	// add_history(str);
-
-	// free(str);
+int main()
+{
+	
 
 }
