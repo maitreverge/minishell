b55a231acf46ded3882c9e0a9dddbe1a597c702c
de_la_verge <verge.f@gmail.com>
2024-02-05 13:52:14 +0100

    🛠️ WORKING ON : implememting another fucking split

--- src/parsing/testing.c
+++ src/parsing/testing.c
@@ -1,5 +1,94 @@
 #include "../../minishell.h"
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 15:21:44 by flverge           #+#    #+#             */
+/*   Updated: 2023/10/06 16:10:26 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	sub_check(char const *s, char c, size_t *i, size_t *start)
+{
+	while (s[*i] == c)
+		(*i)++;
+	if (s[*i] != c && s[*i])
+	{
+		*start = *i;
+		while (s[*i] != c && s[*i])
+			(*i)++;
+	}
+}
+
+static void	allocation(char **buffer, char const *s, char c, size_t len_s)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	start = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		sub_check(s, c, &i, &start);
+		if (j < len_s)
+		{
+			buffer[j] = ft_calloc(sizeof(char), (i - start + 1));
+			if (!buffer[j])
+				return ;
+			ft_strncpy(buffer[j], &s[start], i - start);
+			j++;
+		}
+		while (s[i] == c && s[i])
+			i++;
+	}
+}
+
+static size_t	ft_countwords(char const *str, char c)
+{
+	size_t	result;
+	int		i;
+
+	result = 0;
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		while (str[i] == c && str[i])
+			i++;
+		if (str[i] != c && str[i])
+		{
+			result++;
+			while (str[i] != c && str[i])
+				i++;
+		}
+	}
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**buffer;
+	size_t	len_s;
+
+	len_s = ft_countwords(s, c);
+	buffer = (char **)ft_calloc(sizeof(char *), (len_s + 1));
+	if (!buffer)
+		return (NULL);
+	allocation(buffer, s, c, len_s);
+	return (buffer);
+}
+
 t_env_list	*env_lstnew(char *s_key, char *s_value, char *envp)
 {
 	t_env_list	*new_node;
@@ -7,9 +96,9 @@ t_env_list	*env_lstnew(char *s_key, char *s_value, char *envp)
 	new_node = (t_env_list *)malloc(sizeof(t_env_list));
 	if (!new_node)
 		return (NULL);
+	new_node->original_envp = envp;
 	new_node->key = s_key;
 	new_node->value = s_value;
-	new_node->original_envp = envp;
 	new_node->next = NULL; // add 
 	return (new_node);
 }
@@ -36,7 +125,6 @@ void	env_lstadd_back(t_env_list **lst, t_env_list *new)
 
 	tail = env_lstlast(*lst);
 	tail->next = new;
-	// new->prev = tail;
 }
 
 void	my_copy_env_into_list(t_env_list **env, char **envp)
@@ -57,7 +145,7 @@ void	my_copy_env_into_list(t_env_list **env, char **envp)
 		if (!current)
 		{
 			current = env_lstnew(s_key, s_value, envp[i]);
-			// current->original_envp = envp[i];
+			*env = current;
 		}
 		else
 		{
