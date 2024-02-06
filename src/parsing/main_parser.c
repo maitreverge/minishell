375e204798a6bcd8016d142d44eb145ce2ff72b2
maitreverge <verge.f@gmail.com>
2024-02-06 13:09:53 +0100

    gdb c'est incroyable mec

--- src/parsing/main_parser.c
+++ src/parsing/main_parser.c
@@ -6,12 +6,12 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 07:26:07 by flverge           #+#    #+#             */
-/*   Updated: 2024/02/05 17:12:13 by flverge          ###   ########.fr       */
+/*   Updated: 2024/02/06 13:04:19 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-// #include "../../include/minishell.h"
+
 
 /**
  * @brief My main function for parsing checking only
@@ -26,6 +26,8 @@ int main(int ac, char **av, char **envp)//, char **env
 	t_pars *pars; // mains struct which will contains the parsing output
 	t_env_list *s_env;
 	char *line;
+
+	s_env = NULL;
 	
 	copy_env_into_list(&s_env, envp); // copy env into struct
 	char *post_pars; // temp checker
