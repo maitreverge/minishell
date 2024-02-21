049f85c3a451f693bc62a7c89b52d1171365d0ce
flverge_42 <verge.f@gmail.com>
2024-02-21 10:08:14 +0100

    🔧 SMALL FIX : fixed segfaulting `echo`

--- src/exec/builtins_echo.c
+++ src/exec/builtins_echo.c
@@ -3,23 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_echo.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: glambrig <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:12:13 by glambrig          #+#    #+#             */
-/*   Updated: 2024/02/16 13:25:10 by glambrig         ###   ########.fr       */
+/*   Updated: 2024/02/21 10:07:34 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+/*
+Edge cases to handle :
+
+echo -nnnnnnnnnnnn n
+echo -nnnnnnnnnnnnnn -nnnnnnnnnnnnnnnn hello
+echo -nnnnnnnnnnnnnnn -nnnnnnnnnnnnnnnnnn -n hello
+
+*/
+
 int	ft_echo(t_pars *pars)//char *s, t_all *all, 
 {
 	int	i;
 	int	len;
+	// ! actual *pars points towards the "echo" node, not the first node
+	t_pars *first_node; // add a variable to modify the last_exit_value
+
+	first_node = lstfirst(pars); // first node points indeed towards the first node
 
 	len = 0;
 	while (pars->cmd->name_options_args[len])
 		len++;
+	// ! add exception if prompt is only "echo"
+	if (!pars->cmd->name_options_args[1])
+	{
+		printf("\n");
+		first_node->last_exit_status = 0;
+		return (0);
+	}
+	// this line segfault when echo got no arguments only one arguments
 	if (ft_strncmp(pars->cmd->name_options_args[1], "-n", 2) == 0)
 		i = 2;
 	else
@@ -36,6 +57,6 @@ int	ft_echo(t_pars *pars)//char *s, t_all *all,
 	}
 	if (ft_strncmp(pars->cmd->name_options_args[1], "-n", 2) != 0)
 		printf("\n");
-	pars->prev->last_exit_status = 0;
+	first_node->last_exit_status = 0;
 	return (0);
 }
