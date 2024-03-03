3bc1e76e2ac15de395a679ea46d12705c964d9cf
maitreverge <verge.f@gmail.com>
2024-03-03 18:40:41 +0100

    🚀 MEGA UPDATE : `minishell` not leaking anymore

--- src/exec/pipes.c
+++ src/exec/pipes.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 12:56:20 by glambrig          #+#    #+#             */
-/*   Updated: 2024/03/03 18:23:11 by flverge          ###   ########.fr       */
+/*   Updated: 2024/03/03 18:39:50 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,17 +82,20 @@ int	pipes(t_pars **lst, t_all *all, int input_fd)
 {
 	t_pipe	pippy;
 
+	t_pars	*cur;
+	cur = *lst;
+
 	pippy.fds = create_pipes(lst, &pippy.ch_pid, &pippy.i);
-	while ((*lst) != NULL && (*lst)->is_command == true)
+	while (cur != NULL && cur->is_command == true)
 	{
 		if (pipes_helper_1(lst, all, input_fd) == 1)
 			break ;
 		pippy.ch_pid[pippy.i] = fork();
 		pipes_helper_2(lst, all, pippy, input_fd);
 		input_fd = pippy.fds[pippy.i++][0];
-		if ((*lst)->next && (*lst)->next->is_operator == true
-			&& (*lst)->next->operator->pipe == true)
-			(*lst) = (*lst)->next->next;
+		if (cur->next && cur->next->is_operator == true
+			&& cur->next->operator->pipe == true)
+			cur = cur->next->next; // leaks comes from the old version where you played with the whole struct
 		else
 		{
 			pipes_helper_3(lst, all);
